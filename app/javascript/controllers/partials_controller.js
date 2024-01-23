8d1bc82b7d86d8f54e17f18ee9c2a209ec53fba8
Saulo Oliveira <me@saulo.pro>
2024-01-23 18:59:49 +0000

    Auto focus on edit field

--- app/javascript/controllers/partials_controller.js
+++ app/javascript/controllers/partials_controller.js
@@ -2,8 +2,14 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   static targets = [
-    'form'
+    'form',
+    'input'
   ]
+
+  connect() {
+    this.inputTarget.focus()
+    this.inputTarget.select()
+  }
 
   submitForm() {
     this.formTarget.requestSubmit()

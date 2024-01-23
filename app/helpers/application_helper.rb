06f5f9a14c97170e26858710c10ee0e446eebe8d
Saulo Oliveira <me@saulo.pro>
2024-01-23 18:47:00 +0000

    Create render_field method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def partial_id (model, field_name)
     "#{dom_id(model)}_#{field_name}"
   end
+
+  def render_field (model:, field:)
+    render partial: "partials/show", locals: {model: model, field: field}
+  end
+
 end

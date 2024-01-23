08b34f7cfe33d902cf44de51b090f4d5c235b086
Saulo Oliveira <me@saulo.pro>
2024-01-23 20:23:28 +0000

    Error messages in the field

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,9 @@ module ApplicationHelper
     "#{dom_id(model)}_#{field}"
   end
 
-  def render_field (model:, field:)
+  def render_field (model:, field:, edit_state: false)
+    return render partial: "partials/edit_field", locals: {model: model, field: field} if edit_state
+
     render partial: "partials/show", locals: {model: model, field: field}
   end
 
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,3 @@
 class Person < ApplicationRecord
+  validates :age, numericality: { greater_than_or_equal_to: 0 }
 end
